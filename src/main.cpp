9faf097da974c86f660c7674c4047990c06268b2
diproray <diprorayy@gmail.com>
2018-03-11 13:35:46 -0500

    main.cpp added. Currently used for manual testing.

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+//
+// Created by Dipro Ray on 3/11/18.
+//
+
+#include "image_data.cpp"
+
+int main() {
+
+  ImageData image_object;
+
+  cin >> image_object;
+  cout << std::endl;
+  cout << image_object;
+}
+

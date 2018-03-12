b469c40278e306dc56ebf5a1ace08f6103e80ca3
diproray <diprorayy@gmail.com>
2018-03-12 00:47:34 -0500

    main.cpp updated for manual debugging purposes.

--- src/main.cpp
+++ src/main.cpp
@@ -6,18 +6,13 @@
 #include "training_data.cpp"
 
 int main() {
-  /*
-  ImageData image_object;
-  image_object.SetImageFromFile("trainingimages");
-  //cin >> image_object;
-  cout << std::endl;
-  cout << image_object;
-  */
-
 
   TrainingData training_data_object;
-  training_data_object.GenerateVectorOfLabelsFromFileData("../data/test_labels.txt");
+  training_data_object.GenerateVectorOfImagesFromFileData("../data/trainingimages");
+
+  training_data_object.GenerateVectorOfLabelsFromFileData("../data/traininglabels");
   cout << '\n';
   training_data_object.GenerateVectorOfPriorsForLabels();
+
 }
 

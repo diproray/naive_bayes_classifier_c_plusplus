354140b2d234268cce786ada3e97d8d57c8f39e7
diproray <diprorayy@gmail.com>
2018-03-12 18:57:50 -0500

    JavaDoc and explanatory comments added for ClassifierEvaluator.

--- src/classifier_evaluator/classifier_evaluator.cpp
+++ src/classifier_evaluator/classifier_evaluator.cpp
@@ -5,53 +5,98 @@
 #include <iomanip>
 #include <utility>
 #include "classifier_evaluator.h"
+#include "../utils/utils.h"
+
+// The number of class values.
+// For the given dataset, it is 10.
+
 
 static const int kNumberOfClassValues = 10;
 
+/**
+ * . Function that computes the accuracy and confusion matrix for a given model.
+ *
+ * @param model the model which is to be evaluated
+ */
 void ClassifierEvaluator::ComputeAccuracyAndConfusionMatrix(NaiveBayesClassifyingModel model) {
 
+  // Obtain the test dataset of images and labels.
   ImagesAndLabelsDataset test_data = ImagesAndLabelsDataset("../data/testimages", "../data/testlabels", 0.5);
 
-  for (int i = 0; i < kNumberOfClassValues; i++) {
-    for (int j = 0; j < i; j++) {
-      confusion_matrix_[i][j] = 0;
+  // Initialize all values in the confusion matrix to zero.
+
+  for (int row_index = 0; row_index < kNumberOfClassValues; row_index++) {
+    for (int col_index = 0; col_index < row_index; col_index++) {
+      confusion_matrix_[row_index][col_index] = 0;
     }
   }
 
+  // Initialize the int that keeps track of the number of times the classifier
+  // classified correctly.
+
   int count_of_correct_predictions = 0;
+
+  // Get the total number of images.
 
   unsigned long number_of_images = test_data.GetVectorOfImages().size();
 
+  // Do the following for all possible class values.
   for (unsigned long index = 0; index < number_of_images; index++) {
+
+    // Get the class value predicted by the classifier for the image.
 
     int estimated_class = model.Classifier(test_data.GetVectorOfImages().at(index));
 
-    // cout << "Number: " << index << "| Classified as: " << estimated_class << "| Actually is: "
-    //     << test_data.GetVectorOfLabels().at(index) << std::endl;
+
+    // Check if it matches the actual label/class value.
+    // Add to the count of correct predictions, if yes.
+
     if (estimated_class == test_data.GetVectorOfLabels().at(index)) {
       count_of_correct_predictions++;
     }
 
+    // Increment the respective position in the confusion matrix by one.
+
     confusion_matrix_[test_data.GetVectorOfLabels().at(index)][estimated_class]++;
 
-    PrintProgressBar(index);
+    // Print the progress bar to track status.
+
+    PrintProgressBar(index, number_of_images);
   }
 
+  // Compute the accuracy percentage.
   accuracy_percentage_ = ((double) count_of_correct_predictions / number_of_images) * 100;
+
+  // At this point, the confusion matrix contains the number of events at each position.
+  // This will now be converted into precentages in the below function.
 
   ConvertCountToPercentageInConfusionMatrix();
 
 }
 
+/**
+ * . Function that converts a confusion matrix of number of occurrences of events
+ * into a percentage.
+ */
 void ClassifierEvaluator::ConvertCountToPercentageInConfusionMatrix() {
+
+  // Since the count has to be converted to percentage,
+  // In each row, find the total number of images of that class label.
+  // Then, divide all the values in that row by the total no. and multiply by 100.
+  // This gives us the percentage values.
 
   for (int row_index = 0; row_index < kNumberOfClassValues; row_index++) {
 
     int sum = 0;
 
+    // Compute the total number of images of that class label.
+
     for (int col_index = 0; col_index < kNumberOfClassValues; col_index++) {
       sum += confusion_matrix_[row_index][col_index];
     }
+
+    // Divide all the values in that row by the total no. and multiply by 100.
+    // This gives us the percentages.
 
     for (int col_index = 0; col_index < kNumberOfClassValues; col_index++) {
       confusion_matrix_[row_index][col_index] = (confusion_matrix_[row_index][col_index] / sum) * 100;
@@ -59,24 +104,16 @@ void ClassifierEvaluator::ConvertCountToPercentageInConfusionMatrix() {
   }
 }
 
-void ClassifierEvaluator::PrintProgressBar(unsigned long image_index) {
-
-  int length_of_bar = 50;
-  auto number_of_hashes = (int) (((double) (image_index + 1) / 1000) * length_of_bar);
-  cout << "Status Bar: [|";
-
-  for (int index = 0; index < number_of_hashes; index++) {
-    cout << "#|";
-  }
-
-  for (int index = 0; index < (length_of_bar - number_of_hashes); index++) {
-    cout << " |";
-  }
-
-  cout << "]" << std::endl;
-}
-
+/**
+ * . Function that overloads the << operator. Used mainly for debugging purposes.
+ *
+ * @param output_stream the stream to put the data into
+ * @param classifier_evaluator the object whose data is to be printed
+ * @return the output stream
+ */
 ostream &operator<<(ostream &output_stream, const ClassifierEvaluator &classifier_evaluator) {
+
+  // Print all the relevant details.
 
   cout << '\n' << "Percentage of Accuracy: " << classifier_evaluator.accuracy_percentage_ << "%";
 
@@ -96,7 +133,25 @@ ostream &operator<<(ostream &output_stream, const ClassifierEvaluator &classifie
   return output_stream;
 }
 
+/**
+ * . Constructor for ClassifierEvaluator object.
+ *
+ * @param model the classifier model to be evaluated
+ */
 ClassifierEvaluator::ClassifierEvaluator(NaiveBayesClassifyingModel model) {
 
+  // Compute accuracy % and the confusion matrix for the model.
+
   ComputeAccuracyAndConfusionMatrix(std::move(model));
+}
+
+/**
+ * . Getter for the object's accuracy percentage.
+ * @return the accuracy percentage
+ */
+double ClassifierEvaluator::GetAccuracyPercentage() {
+
+  // Return the accuracy percentage.
+
+  return accuracy_percentage_;
 }
--- src/classifier_evaluator/classifier_evaluator.h
+++ src/classifier_evaluator/classifier_evaluator.h
@@ -6,25 +6,62 @@
 #define NAIVEBAYES_EVALUATOR_H
 
 #include "../naive_bayes_classifying_model/naive_bayes_classifying_model.h"
+
+/**
+ * . Class that evaluates the accuracy of a classifier.
+ *
+ * @author diproray
+ */
 class ClassifierEvaluator {
 
  private:
 
+  // The accuracy percentage of the classifier.
+
   double accuracy_percentage_;
+
+  // The confusion matrix of the classifier.
+
   double confusion_matrix_[10][10];
 
  public:
 
+  // The default constructor.
   ClassifierEvaluator() = default;
 
+  /**
+   * . Constructor for ClassifierEvaluator object.
+   *
+   * @param model the classifier model to be evaluated
+   */
   explicit ClassifierEvaluator(NaiveBayesClassifyingModel model);
 
+  /**
+   * . Function that computes the accuracy and confusion matrix for a given model.
+   *
+   * @param model the model which is to be evaluated
+   */
   void ComputeAccuracyAndConfusionMatrix(NaiveBayesClassifyingModel model);
 
-  void PrintProgressBar(unsigned long index);
-
+  /**
+   * . Function that converts a confusion matrix of number of occurrences of events
+   * into a percentage.
+   */
   void ConvertCountToPercentageInConfusionMatrix();
 
+  /**
+   * . Getter for the object's accuracy percentage.
+   * @return the accuracy percentage
+   */
+  double GetAccuracyPercentage();
+
+  /**
+   * . Function that overloads the << operator. Used mainly for debugging purposes.
+   *
+   * @param output_stream the stream to put the data into
+   * @param classifier_evaluator the object whose data is to be printed
+   * @return the output stream
+   */
   friend ostream &operator<<(ostream &output_stream, const ClassifierEvaluator &classifier_evaluator);
 
 };

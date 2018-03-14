5b25905c1c0aa0b36af31219d01f218ebe146205
diproray <diprorayy@gmail.com>
2018-03-13 20:28:59 -0500

    Commands for UI saved as constants, comments added in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -22,6 +22,20 @@ int main() {
 
 void interface() {
 
+  // All allowed commands.
+
+  const string kTrainCommand = "train";
+  const string kYesCommand = "yes";
+  const string kSaveCommand = "save";
+  const string kLoadCommand = "load";
+  const string kClassifyCommand = "classify";
+  const string kImproveCommand = "improve";
+  const string kExitCommand = "exit";
+  const string kResetEncodingsCommand = "reset-encodings";
+  const string kResetPixelsFeatureCommand = "reset-pixelsfeature";
+
+  // Set exit interface? to false
+
   bool exit_interface = false;
 
   // Declare the classifier, evaluator, and improver.
@@ -39,13 +53,17 @@ void interface() {
     string command;
     cin >> command;
 
-    if (command == "read") {
+    // Get the command in a string and find out which command it is.
+
+    if (command == kTrainCommand) {
+
+      // Ask user if he/she wants to use the default dataset.
 
       cout << '\n' << "Use default dataset? " << '\n';
 
       cin >> command;
 
-      if (command == "yes") {
+      if (command == kYesCommand) {
 
         model = NaiveBayesClassifyingModel("../data/trainingimages", "../data/traininglabels", 0.1);
 
@@ -63,32 +81,44 @@ void interface() {
 
         model = NaiveBayesClassifyingModel(filename_for_images, filename_for_labels, value);
       }
-    } else if (command == "save") {
+    } else if (command == kSaveCommand) {
+
+      // Save the data of the model
 
       model.SaveModelFeatureProbabilityMatrixAndPriorsVectorToFiles("../data/probabilities", "../data/priors");
 
-    } else if (command == "load") {
+    } else if (command == kLoadCommand) {
+
+      // Load the data for the model
 
       model.LoadModelFeatureProbabilityMatrixAndPriorsVectorFromFiles("../data/probabilities", "../data/priors");
 
-    } else if (command == "classify") {
+    } else if (command == kClassifyCommand) {
+
+      // Evaluate the model through a ClassifierEvaluator object.
 
       evaluator = ClassifierEvaluator(model);
 
+      // Print out the results.
+      // Note: << is overloaded here.
+
       cout << evaluator;
-    } else if (command == "improve") {
+    } else if (command == kImproveCommand) {
+
+      // Find the best k value.
 
       cout << '\n' << "The best k value is" << improver.FindBestKValue() << ".\n";
 
-    } else if (command == "exit") {
+    } else if (command == kResetEncodingsCommand) {
 
-      exit_interface = true;
-    } else if (command == "reset-encodings") {
+      // Ask for the new value of number of different encodings.
 
       cout << '\n' << "How many encodings would you like for the image as a 2D array? 2 or 3?" << '\n';
 
       int encodings;
       cin >> encodings;
+
+      // Accept the encoding value only if it is 2 or 3 or print an error message.
 
       if (encodings == 2 || encodings == 3) {
         ::number_of_different_encodings = encodings;
@@ -97,12 +127,16 @@ void interface() {
         cout << "Inavlid entry, please try again.";
       }
 
-    } else if (command == "reset-pixelsfeature") {
+    } else if (command == kResetPixelsFeatureCommand) {
+
+      // Ask for the new pixel feature size.
 
       cout << '\n' << "How many pixels would you like to be counted in a feature? Enter an even number < 14, or 1." << '\n';
 
       int pixel_dimensions;
       cin >> pixel_dimensions;
+
+      // Accept it only if it meets the criteria: even no. <= 14, or 1.
 
       if ((pixel_dimensions < 14 && pixel_dimensions % 2 == 0) || (pixel_dimensions == 1)) {
         ::pixel_dimension_per_feature = pixel_dimensions;
@@ -111,6 +145,11 @@ void interface() {
         cout << "Inavlid entry, please try again.";
       }
 
+    } else if (command == kExitCommand) {
+
+      // Set exit interface? to true
+
+      exit_interface = true;
     } else {
 
       cout << '\n' << "I do not understand the command.";
